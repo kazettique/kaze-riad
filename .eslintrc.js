8edc8b3fe366b04940b9ae75b5a339f9df503dcc
Woody <woody.chang@cloud-interactive.com>
2020-08-18 18:53:12 +0800

    Update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,34 @@
 module.exports = {
-  extends: 'standard-with-typescript',
+  root: true,
+  parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: './tsconfig.json'
-  }
+    project: './tsconfig.json',
+  },
+  env: {
+    browser: true,
+    node: true,
+  },
+  extends: [
+    'standard-with-typescript',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:jsx-a11y/recommended',
+    // Prettier plugin and recommended rules
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
+  ],
+  rules: {
+    // allow debugger during development
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    // allow trailing commas
+    'comma-dangle': ['error', 'always-multiline'],
+    // for react-hooks lint
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'prettier/prettier': ['error'],
+    // 'no-use-before-define': 'off',
+    '@typescript-eslint/no-use-before-define': ['error', { functions: false }],
+  },
 }
--- src/layout/components/MotionRoute.tsx
+++ src/layout/components/MotionRoute.tsx
@@ -6,30 +6,24 @@ const pageVariants: Variants = {
   initial: {
     x: '100%',
     opacity: 1,
-    transition: { duration: 0.5 }
+    transition: { duration: 0.5 },
   },
   in: {
     x: 0,
-    opacity: 1
+    opacity: 1,
   },
   out: {
     opacity: 1,
     x: '-100%',
-    transition: { duration: 0.5 }
-  }
+    transition: { duration: 0.5 },
+  },
 }
 
-const MotionRoute: React.FC<RouteProps> = (props) => {
+const MotionRoute: React.FC<RouteProps> = ({ children, ...props }) => {
   return (
     <Route {...props}>
-      <motion.div
-        className="flex-grow flex"
-        initial="initial"
-        animate="in"
-        exit="out"
-        variants={pageVariants}
-      >
-        {props.children}
+      <motion.div className='flex-grow flex' initial='initial' animate='in' exit='out' variants={pageVariants}>
+        {children}
       </motion.div>
     </Route>
   )
