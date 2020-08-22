1bb804b602b379a7f83dd19c24ad023ad3db2bac
Woody <woody.chang@cloud-interactive.com>
2020-08-22 15:05:03 +0800

    Adjust page transition

--- src/layout/components/MotionRoute.tsx
+++ src/layout/components/MotionRoute.tsx
@@ -4,25 +4,24 @@ import { motion, Variants } from 'framer-motion'
 
 const pageVariants: Variants = {
   initial: {
-    x: '100%',
-    opacity: 1,
-    transition: { duration: 0.5 },
+    x: '12%',
+    opacity: 0,
   },
   in: {
     x: 0,
     opacity: 1,
   },
   out: {
-    opacity: 1,
-    x: '-100%',
-    transition: { duration: 0.5 },
+    opacity: 0,
+    y: 30,
+    transition: { duration: 0.15 },
   },
 }
 
 const MotionRoute: React.FC<RouteProps> = ({ children, ...props }) => {
   return (
     <Route {...props}>
-      <motion.div className='flex-grow flex' initial='initial' animate='in' exit='out' variants={pageVariants}>
+      <motion.div className='flex-grow flex relative' initial='initial' animate='in' exit='out' variants={pageVariants}>
         {children}
       </motion.div>
     </Route>
